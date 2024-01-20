6803ea4f8b81b81cfe83c35d9ab82e29bed56bbe
pradyumna-bhowmick <pradyumna.bhowmick.2016@gmail.com>
2024-01-20 16:44:59 +0530

    Fixed errors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from '../../../emf/frontend/src/App';
-import reportWebVitals from '../../../emf/frontend/src/reportWebVitals';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
